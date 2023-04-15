d4ee79d592d61cf1aa38705c44939d2e4f431707
Xiaoliang Luan <helix_ogg@hotmail.com>
2023-04-15 14:43:27 +0800

    replace tailwind CDN

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'serif': ['Fraunces', 'serif'],
+      'sans': ['Montserrat', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'dark-cyan': 'hsl(158, 36%, 37%)',
+        'very-dark-cyan': 'hsl(158, 36%, 21%)',
+        'cream': 'hsl(30, 38%, 92%)',
+        'very-dark-blue': 'hsl(212, 21%, 14%)',
+        'dark-grayish-blue': 'hsl(228, 12%, 48%)',
+      }
+    }
   },
   plugins: [],
 }
-
